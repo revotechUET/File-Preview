8737a6de88aeddffde2a2bb17a74176710100256
phucnv282 <phucnv282@gmail.com>
2019-11-04 10:15:01 +0700

    update

--- Dockerfile
+++ Dockerfile
@@ -2,10 +2,13 @@ FROM python:2.7.17-alpine
 
 WORKDIR /app
 
+ENV BUDGET_URL=https://users.i2g.cloud
+ENV SERVICE=WI_PROJECT_STORAGE
+
 COPY . .
 
 RUN pip install -r requirements.txt
 
 EXPOSE 5000
 
-CMD ["gunicorn", "-w 1", "-b 0.0.0.0:5000", "app:app"]
+CMD ["gunicorn", "-b 0.0.0.0:5000", "app:app"]
--- sendRequest.py
+++ sendRequest.py
@@ -12,10 +12,10 @@ config.read('config.ini')
 
 
 def SendRequest(headers, params):
-    env = os.getenv('ENV')
-    budget_url = config['BUDGET_URL']['dev']
-    if env == 'prod':
-        budget_url = config['BUDGET_URL']['prod']
+    budget_url = os.getenv('BUDGET_URL') or config['BUDGET_URL']['url'] or "https://users.i2g.cloud"
+    service_name = ""
+    if os.getenv('SERVICE'):
+        service_name = "&service={}".format(os.getenv('SERVICE'))
     res = requests.get(
-        "{}/read-file/preview?file_path={}&service=WI_PROJECT_STORAGE".format(budget_url, requote_uri(params)), headers=headers, verify=False)
+        "{}/read-file/preview?file_path={}{}".format(budget_url, requote_uri(params), service_name), headers=headers, verify=False)
     return res
